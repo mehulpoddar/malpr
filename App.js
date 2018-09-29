df3694dbf233decb49e9a0eaed644268f8057333
bhargav0528 <bhargavauden1998@gmail.com>
2018-09-29 10:29:48 +0530

    Camera functionality added

--- App.js
+++ App.js
@@ -7,43 +7,20 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
+import PlateDetector from './src/components/ALPR/PlateDetector';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component{
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={{flex:1}}>
+        <View style={{flex:1, justifyContent:"center", alignItems:"center", elevation:11,backgroundColor:'#fff', shadowColor:'#000', shadowOffset: {width:2, height:2}, shadowOpacity:0.2}}>
+          <Text style={{fontSize:18, color:'#000'}}>MALPR</Text>
+        </View>
+        <View style={{flex:10}}>
+        <PlateDetector />
+        </View>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
