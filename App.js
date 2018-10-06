e8373ae8fbe1b18370a4792cb8f48a5da0f1aae2
bhargav0528 <bhargavauden1998@gmail.com>
2018-10-06 18:41:43 +0530

    ALPR Basic Fuctionality

--- App.js
+++ App.js
@@ -9,8 +9,23 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import PlateDetector from './src/components/ALPR/PlateDetector';
+import firebase from 'firebase';
 
 export default class App extends Component{
+  componentWillMount(){
+    // Initialize Firebase
+    if (!firebase.apps.length) {
+      const config = {
+        apiKey: 'AIzaSyBZYTCnfIfMOB8NE3Kqpnc91VV4US1c-Ug',
+        authDomain: 'dsceapp-5ed7f.firebaseapp.com',
+        databaseURL: 'https://dsceapp-5ed7f.firebaseio.com',
+        projectId: 'dsceapp-5ed7f',
+        storageBucket: 'dsceapp-5ed7f.appspot.com',
+        messagingSenderId: '359041235154',
+      };
+      firebase.initializeApp(config);
+    }
+  }
   render() {
     return (
       <View style={{flex:1}}>
--- src/components/ALPR/PlateDetector.js
+++ src/components/ALPR/PlateDetector.js
@@ -5,14 +5,98 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
-  View
+  View,
+  Image,
+  ToastAndroid,
+  Platform,
+  ActivityIndicator
 } from 'react-native';
 import { RNCamera } from 'react-native-camera';
+import RNFetchBlob from 'react-native-fetch-blob';
+import firebase from 'firebase';
 
 export default class PlateDetector extends Component{
-    render(){
-        return(
-            <View style={styles.container}>
+
+    state = {clicked:false , imageuri:'', processing:false, plateText:'', flaskurl:''}
+
+    cleanUrl(url){
+      
+      url = url.replace('%2F','%252F')
+      return url
+    }
+
+
+    async getNumPlate() {
+      console.log("processing")
+      try {
+        url = this.state.flaskurl
+        url = this.cleanUrl(url)
+        console.log("URLLLLLLLLLLL",url)
+        let response = await fetch(
+          'http://10.0.2.2:5000/plate?url='+url
+        );
+        console.log("Banthu")
+        js = await response.json()
+        console.log(js.plate);
+        this.setState({processing:true, plateText:js.plate})
+      } catch (error) {
+        console.error(error);
+      }
+    }
+   
+
+    uploadDetails()
+    {
+      const Blob = RNFetchBlob.polyfill.Blob
+      const fs = RNFetchBlob.fs
+      window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest
+      window.Blob = Blob
+    const uploadImage = (uri, imageName, mime = 'image') => {
+    return new Promise((resolve, reject) => {
+      const uploadUri = Platform.OS === 'ios' ? uri.replace('file://', '') : uri
+        let uploadBlob = null
+      const imageRef = firebase.storage().ref('posts').child(imageName)
+        fs.readFile(uploadUri, 'base64')
+        .then((data) => {
+          return Blob.build(data, { type: `${mime};BASE64` })
+        })
+        .then((blob) => {
+          uploadBlob = blob
+          return imageRef.put(blob, { contentType: mime })
+        })
+        .then(() => {
+          uploadBlob.close()
+          console.log(imageRef.getDownloadURL)
+          return imageRef.getDownloadURL()
+        })
+        .then((url) => {
+          //firebase.database().ref(`branch/${this.state.branch}/notes/sem_${this.state.sem}/${this.state.subject}`).push().key
+          
+              //ToastAndroid.show('Processing', ToastAndroid.SHORT);
+              console.log(url)
+              this.setState({processing:null, flaskurl:url},()=>{
+                this.getNumPlate()
+              })
+
+              resolve(url)
+            });
+            
+        
+          
+        })
+        
+  }
+     ToastAndroid.show('Processing, Please Wait...',ToastAndroid.SHORT)
+     //console.log('chapName', this.state.chap+`${this.state.mimeType.split("/")[1]}`)
+     g= uploadImage(this.state.imageuri, "Hello2" , ".jpg")
+  
+    }
+
+
+    cameraOrPic(){
+      if(!this.state.clicked)
+      {
+        return <View style={styles.container}> 
         <RNCamera
             ref={ref => {
               this.camera = ref;
@@ -26,15 +110,72 @@ export default class PlateDetector extends Component{
               console.log(barcodes)
             }}
         />
-        <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center',}}>
+        <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center',backgroundColor:'#fff'}}>
         <TouchableOpacity
             onPress={this.takePicture.bind(this)}
             style = {styles.capture}
         >
-            <Text style={{fontSize: 14}}> SNAP </Text>
+            <Text style={{fontSize: 14,color:'#fff'}}> SNAP </Text>
         </TouchableOpacity>
         </View>
-      </View>
+        </View>
+        
+      }
+      else{
+        if(this.state.imageuri!='' && this.state.processing==false)
+        {
+          return <View>
+          <Image style={{width:'100%', height:'85%'}} source={{isStatic:true, uri:this.state.imageuri}} />
+          <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center',backgroundColor:'#fff'}}>
+        <TouchableOpacity
+            onPress={this.uploadDetails.bind(this)}
+            style = {styles.capture}
+        >
+            <Text style={{fontSize: 14,color:'#fff'}}> YES </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+            onPress={()=>{this.setState({clicked:false})}}
+            style = {styles.capture}
+        >
+            <Text style={{fontSize: 14,color:'#fff'}}> NO </Text>
+        </TouchableOpacity>
+        </View>
+          </View>
+
+        }
+        else if(this.state.processing==null)
+        {
+          return <View>
+          <Image style={{width:'100%', height:'85%'}} source={{isStatic:true, uri:this.state.imageuri}} />
+          <View style={{width:'100%', height:'15%', flexDirection: 'row', justifyContent: 'center',backgroundColor:'#fff'}}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+          </View>
+          
+          
+        }
+        else if(this.state.processing==true)
+        { 
+          return <View>
+          <Image style={{width:'100%', height:'85%'}} source={{isStatic:true, uri:this.state.imageuri}} />
+          <View style={{width:'100%', height:'15%', flexDirection: 'row', justifyContent: 'center',backgroundColor:'#fff'}}>
+          <View style={{flex:1}}>
+            <Text style={{
+      alignSelf: 'center', fontSize:18, color:'#000'}}>Number Plate</Text>
+            <Text style={{
+      alignSelf: 'center', fontSize:18, color:'#000'}}>{this.state.plateText}</Text>
+            </View>
+        </View>
+          </View>
+        }
+      }
+    }
+
+    render(){
+        return(
+            <View style={styles.container}>
+            {this.cameraOrPic()}
+           </View>
         )
     }
     takePicture = async function() {
@@ -42,8 +183,10 @@ export default class PlateDetector extends Component{
           const options = { quality: 0.5, base64: true };
           const data = await this.camera.takePictureAsync(options)
           console.log(data.uri);
+          this.setState({clicked:true, imageuri:data.uri})
         }
       }
+      
     }
 
 
@@ -61,7 +204,7 @@ const styles = StyleSheet.create({
     },
     capture: {
       flex: 0,
-      backgroundColor: '#fff',
+      backgroundColor: '#000',
       borderRadius: 5,
       padding: 15,
       paddingHorizontal: 20,
@@ -69,4 +212,3 @@ const styles = StyleSheet.create({
       margin: 20
     }
   });
-  
