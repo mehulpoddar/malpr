3478594458ff67b9a6c55b1ce683d02c434dcd58
mehulpoddar <mehulpoddar@gmail.com>
2019-01-18 05:13:34 +0530

    what?

--- src/components/ALPR/PlateDetector.js
+++ src/components/ALPR/PlateDetector.js
@@ -14,11 +14,7 @@ import {
 import { RNCamera } from 'react-native-camera';
 import RNFetchBlob from 'react-native-fetch-blob';
 import firebase from 'firebase';
-<<<<<<< HEAD
-import CropSelector from 'react-crop-selector';
-=======
 import { RNDocScanner } from 'rn-doc-scanner'
->>>>>>> 911ad1267ed8aac724b74fd08a8ace9cfc7641c0
 
 // symbol polyfills
 global.Symbol = require('core-js/es6/symbol');
@@ -47,12 +43,8 @@ export default class PlateDetector extends Component{
         url = this.cleanUrl(url)
         console.log("URLLLLLLLLLLL",url)
         let response = await fetch(
-<<<<<<< HEAD
-          'http://192.168.1.43.111:5000/plate?url='+url
-=======
           'http://192.168.43.111:5000/plate?url='+url
           //'http://192.168.1.7:5000/plate?url=https://i.ibb.co/7RLK4PM/test1.jpg'
->>>>>>> 911ad1267ed8aac724b74fd08a8ace9cfc7641c0
         );
         console.log("Banthu")
         js = await response.json()
@@ -157,15 +149,7 @@ export default class PlateDetector extends Component{
         if(this.state.imageuri!='' && this.state.processing==false)
         {
           return <View>
-<<<<<<< HEAD
-          <CropSelector
-            width={640} height={480}
-            x1={5} y1={5} x2={95} y2={95}
-          />
-          <Image style={{width:'100%', height:'85%'}} source={{isStatic:true, uri:this.state.imageuri}} />
-=======
           <Image source={{isStatic:true, uri: this.state.testImage }} style={{width:'100%', height:'85%'}} />
->>>>>>> 911ad1267ed8aac724b74fd08a8ace9cfc7641c0
           <View style={{flex: 0, flexDirection: 'row', justifyContent: 'center',backgroundColor:'#fff'}}>
         <TouchableOpacity
             onPress={this.uploadDetails.bind(this)}
